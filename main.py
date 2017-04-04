c02aa112b559234034882d8d332605902f98539a
Anna Youngyeun <anna.youngyeun@gmail.com>
2017-04-04 13:27:50 -0500

    Regular expression checks added.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ h1{
 }
 form{
     font-family: Helvetica;
+    font-style: oblique;
 
 }
 tr, td {
@@ -28,7 +29,6 @@ table {
 </style>
 </head>
 <body>
-    <h1> Signup </h1>
 """
 
 page_footer = """
@@ -81,14 +81,18 @@ class Index(webapp2.RequestHandler):
                 <input type = "submit"/>
             </form>
         """.format(username, email) # value leaves form populated if value exists  .format leaves user submitted values to keep fields populated
-        content = page_header + signup_form + page_footer
+        content = page_header + "<h1> Signup </h1>" + signup_form + page_footer
         self.response.write(content)
 
 class Welcome(webapp2.RequestHandler):
     """ Handles requests coming from form """
+
     def get(self):
         username = self.request.get("username")
         self.response.write("<h1> Welcome, " + username + "! </h1>")
+
+        content = page_header + page_footer
+        self.response.write(content)
 
     def post(self):
         #retrieve user input
@@ -97,12 +101,33 @@ class Welcome(webapp2.RequestHandler):
         verifypassword = self.request.get("verifypassword")
         email = self.request.get("email")
 
-        #check if username, pw, or verify are left blank.  redirects to form
+        username_checks = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        username_okay = username_checks.match(username)
+
+        email_checks = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+        email_okay = email_checks.match(email)
+
+        password_checks = re.compile(r"^.{3,20}$")
+        password_okay = password_checks.match(password)
+
         if username == "" or password == "" or verifypassword =="":
+            self.redirect("/")
+
+        elif password != verifypassword:
+            self.redirect("/")
+
+        elif not username_okay:
+            self.redirect("/")
+
+        elif not email_okay:
+            self.redirect("/")
+
+        elif not password_okay:
             self.redirect("/")
 
         else:
             self.redirect("/welcome?username=" + username)
+
 
 
 
