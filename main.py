0c060c62b0b5c01b41df0ecd7208770aea694250
Anna Youngyeun <anna.youngyeun@gmail.com>
2017-04-04 15:31:27 -0500

    Reworked to place checks in Index handler

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ page_header = """
 <style>
 h1{
     font-family: Helvetica;
+    font-style: oblique;
     color: #4bdca3;
 }
 form{
@@ -38,17 +39,20 @@ page_footer = """
 
 class Index(webapp2.RequestHandler):
     """ Handles requests coming to '/' (root site for signup)"""
+
     def get(self):
+
         username = self.request.get("username")
         email = self.request.get("email")
         signup_form = """
-            <form action = "/welcome" method = "post">
+            <form method = "post">
                 <table style="width:100%">
                     <tr>
                         <td>
                             <label>
                                 Username:
                                 <input type = "text" name = "username" value="{0}"/>
+                                <span class = error>Please enter a valid username.</span>
                             <label>
                         </td>
                     </tr>
@@ -57,6 +61,7 @@ class Index(webapp2.RequestHandler):
                             <label>
                                 Password:
                                 <input type = "password" name = "password"/>
+                                <span class = error>That's not a valid password.</span>
                             </label>
                         </td>
                     </tr>
@@ -65,6 +70,7 @@ class Index(webapp2.RequestHandler):
                             <label>
                                 Verify Password:
                                 <input type = "password" name = "verifypassword"/>
+                                <span class = error> Passwords don't match. </span>
                             </label>
                         </td>
                     </tr>
@@ -73,6 +79,7 @@ class Index(webapp2.RequestHandler):
                             <label>
                                 Email (Optional):
                                 <input type = "text" name = "email" value="{1}"/>
+                                <span class = error>Please enter a valid email.</span>
                             </label>
                         </td>
                     </tr>
@@ -81,17 +88,9 @@ class Index(webapp2.RequestHandler):
                 <input type = "submit"/>
             </form>
         """.format(username, email) # value leaves form populated if value exists  .format leaves user submitted values to keep fields populated
+
+
         content = page_header + "<h1> Signup </h1>" + signup_form + page_footer
-        self.response.write(content)
-
-class Welcome(webapp2.RequestHandler):
-    """ Handles requests coming from form """
-
-    def get(self):
-        username = self.request.get("username")
-        self.response.write("<h1> Welcome, " + username + "! </h1>")
-
-        content = page_header + page_footer
         self.response.write(content)
 
     def post(self):
@@ -111,16 +110,16 @@ class Welcome(webapp2.RequestHandler):
         password_okay = password_checks.match(password)
 
         if username == "" or password == "" or verifypassword =="":
-            self.redirect("/")
+            self.redirect("/?empty")
 
         elif password != verifypassword:
-            self.redirect("/")
+            self.redirect("/?password")
 
         elif not username_okay:
-            self.redirect("/")
+            self.redirect("/?baduser")
 
         elif not email_okay:
-            self.redirect("/")
+            self.redirect("/?bademail")
 
         elif not password_okay:
             self.redirect("/")
@@ -128,7 +127,15 @@ class Welcome(webapp2.RequestHandler):
         else:
             self.redirect("/welcome?username=" + username)
 
+class Welcome(webapp2.RequestHandler):
+    """ Handles requests coming from form """
 
+    def get(self):
+        username = self.request.get("username")
+        self.response.write("<h1> Welcome, " + username + "! </h1>")
+
+        content = page_header + page_footer
+        self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
