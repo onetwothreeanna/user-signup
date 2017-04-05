46a8e81e1ef2b5ae31c1fcc636563b5274949cf4
Anna Youngyeun <anna.youngyeun@gmail.com>
2017-04-04 23:44:28 -0500

    Turned form into global variable.  Working, but ugly.

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ page_header = """
 <head>
     <title> User Signup </title>
 
-
 <style>
 h1{
     font-family: Helvetica;
@@ -27,6 +26,13 @@ tr, td {
 table {
     width: 100%;
 }
+
+.error{
+    font-family: Helvetica;
+    font-size: 14px;
+    font-style: oblique;
+    color: navy;
+}
 </style>
 </head>
 <body>
@@ -37,69 +43,84 @@ page_footer = """
 </html>
 """
 
+#Global function to build the form, taking in all values as parameters
+def buildForm(username, email, error_username, error_password, error_verify, error_email):
+    signup_form = """
+        <form method = "post">
+            <table style="width:100%">
+                <tr>
+                    <td>
+                        <label>
+                            Username:
+                            <input type = "text" name = "username" value = "{0}"/>
+                            <span class = error>{2}</span>
+                        <label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>
+                            Password:
+                            <input type = "password" name = "password"/>
+                            <span class = error>{3}</span>
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>
+                            Verify Password:
+                            <input type = "password" name = "verifypassword"/>
+                            <span class = error> {4} </span>
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>
+                            Email (Optional):
+                            <input type = "text" name = "email" value = "{1}"/>
+                            <span class = error>{5}</span>
+                        </label>
+                    </td>
+                </tr>
+
+            </table>
+            <input type = "submit"/>
+        </form>
+    """.format(username, email, error_username, error_password, error_verify, error_email)
+    return signup_form
+
+
+
 class Index(webapp2.RequestHandler):
     """ Handles requests coming to '/' (root site for signup)"""
-
+#Upon request, build page with empty form
     def get(self):
-
-        username = self.request.get("username")
-        email = self.request.get("email")
-        signup_form = """
-            <form method = "post">
-                <table style="width:100%">
-                    <tr>
-                        <td>
-                            <label>
-                                Username:
-                                <input type = "text" name = "username" value="{0}"/>
-                                <span class = error>Please enter a valid username.</span>
-                            <label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>
-                                Password:
-                                <input type = "password" name = "password"/>
-                                <span class = error>That's not a valid password.</span>
-                            </label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>
-                                Verify Password:
-                                <input type = "password" name = "verifypassword"/>
-                                <span class = error> Passwords don't match. </span>
-                            </label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>
-                                Email (Optional):
-                                <input type = "text" name = "email" value="{1}"/>
-                                <span class = error>Please enter a valid email.</span>
-                            </label>
-                        </td>
-                    </tr>
-
-                </table>
-                <input type = "submit"/>
-            </form>
-        """.format(username, email) # value leaves form populated if value exists  .format leaves user submitted values to keep fields populated
-
-
-        content = page_header + "<h1> Signup </h1>" + signup_form + page_footer
+        username = ""
+        email = ""
+        error_username = ""
+        error_password = ""
+        error_verify = ""
+        error_email = ""
+        form = buildForm(username, email, error_username, error_password, error_verify, error_email)
+        title = '<h1> Signup </h1>'
+        content = (page_header + title + form + page_footer)
         self.response.write(content)
 
+#Once submitted, retrieve input from user and start with empty errors
     def post(self):
         #retrieve user input
         username = self.request.get("username")
         password = self.request.get("password")
         verifypassword = self.request.get("verifypassword")
         email = self.request.get("email")
+        error_username = ""
+        error_password = ""
+        error_verify = ""
+        error_email = ""
 
+        #check for regular expressions
         username_checks = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
         username_okay = username_checks.match(username)
 
@@ -109,32 +130,44 @@ class Index(webapp2.RequestHandler):
         password_checks = re.compile(r"^.{3,20}$")
         password_okay = password_checks.match(password)
 
-        if username == "" or password == "" or verifypassword =="":
-            self.redirect("/?empty")
+        #check for plethora of errors, default to no errors
+        errors = False
+
+        #if errors = True, update error name to include message.
+        if username == "" or not username_okay:
+            error_username= "Please enter a valid username."
+            errors = True
+
+        elif password == "" or not password_okay:
+            error_password=  "Please enter a valid password."
+            errors = True
 
         elif password != verifypassword:
-            self.redirect("/?password")
+            error_verify=  "Passwords do not match."
+            errors = True
 
-        elif not username_okay:
-            self.redirect("/?baduser")
+        elif email != "" and not email_okay:
+            error_email= "Please enter a valid email address."
+            errors = True
 
-        elif not email_okay:
-            self.redirect("/?bademail")
-
-        elif not password_okay:
-            self.redirect("/")
-
-        else:
+        #if no errors, send to welcome page, including username
+        if errors == False:
             self.redirect("/welcome?username=" + username)
+
+        #if errors, rebuild form with populated fields using updated values
+        else:
+            form = buildForm(username, email, error_username, error_password, error_verify, error_email)
+            title = '<h1> Signup </h1>'
+            content = (page_header + title + form + page_footer)
+            self.response.write(content)
 
 class Welcome(webapp2.RequestHandler):
     """ Handles requests coming from form """
 
     def get(self):
         username = self.request.get("username")
-        self.response.write("<h1> Welcome, " + username + "! </h1>")
-
-        content = page_header + page_footer
+        message = "<h1> Welcome, " + username + "! </h1>"
+        content = page_header + message + page_footer
         self.response.write(content)
 
 
